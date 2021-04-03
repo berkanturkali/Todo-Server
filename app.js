99dac914cb6afbbe49f3de9ff9c5f9b4ea0f020b
berkanturkali <brktrkali@outlook.com>
2021-04-03 22:31:47 +0300

    user model

---
+++ app.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = express();
+const port = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URI;
+
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"));
+}
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const fileFilter = (req,file,cb) => {
+    if(file.mimetype == 'image/*'){
+      cb(null,true);
+    }else{
+      cb(null,false);
+    }
+  }
+
+  app.use(express.json());
+  app.use(multer({storage:fileStorage,fileFilter:fileFilter}).single('image'));
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  app.use((err, req, res, next) => {
+    console.log(err);
+    const status = err.statusCode || 500;
+    const message = err.message;
+    const data = err.data;
+    res.status(status).json({ status:status,message: message });
+  });
+
+  mongoose.set('useNewUrlParser',true);
+  mongoose.set('useFindAndModify',false);
+  mongoose.set('useUnifiedTopology',true);
+
+  mongoose.connect(MONGO_URL)
+  .then((result) =>{
+    app.listen(port,() =>{
+      console.log(`Listening on port ${port}`);
+  });
+  }).catch((err) =>{
+    console.log(`Something went wrong ${err}`);
+  });
